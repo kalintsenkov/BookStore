69c372c9899fd0eb5437f2ed5a74f749b8b2bbfc
kalintsenkov <kalintsenkov3@gmail.com>
2022-07-28 15:28:39 +0300

    Added serilog

--- src/BookStore.Startup/Program.cs
+++ src/BookStore.Startup/Program.cs
@@ -2,6 +2,7 @@ namespace BookStore.Startup;
 
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Serilog;
 
 public class Program
 {
@@ -13,6 +14,11 @@ public class Program
     public static IHostBuilder CreateHostBuilder(string[] args)
         => Host
             .CreateDefaultBuilder(args)
+            .UseSerilog((context, services, logger) => logger
+                .ReadFrom.Configuration(context.Configuration)
+                .ReadFrom.Services(services)
+                .Enrich.FromLogContext()
+                .WriteTo.Console())
             .ConfigureWebHostDefaults(webBuilder => webBuilder
                 .UseStartup<Startup>());
 }
