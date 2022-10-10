e376e7e6dc8e169c1348c530f5dde510e09ad985
Kalin Tsenkov <kalintsenkov3@gmail.com>
2022-10-10 12:04:04 +0300

    Added sales books initial data

---
+++ src/BookStore.Domain/Sales/Models/Books/Book.Data.cs
@@ -0,0 +1,16 @@
+﻿namespace BookStore.Domain.Sales.Models.Books;
+
+using System;
+using System.Collections.Generic;
+using Common;
+
+internal class BookData : IInitialData
+{
+    public Type EntityType => typeof(Book);
+
+    public IEnumerable<object> GetData()
+        => new List<Book>
+        {
+            new(title: "It", price: 19.99M, quantity: 100)
+        };
+}
